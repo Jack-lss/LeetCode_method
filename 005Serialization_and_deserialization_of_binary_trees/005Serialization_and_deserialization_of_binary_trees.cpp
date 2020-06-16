3217a5f0214c05b3423b8a8b6022750f38c1024b
Jack-lss <1349916452@qq.com>
2020-06-16 15:02:29 +0800

    +005

---
+++ 005Serialization_and_deserialization_of_binary_trees/005Serialization_and_deserialization_of_binary_trees.cpp
@@ -0,0 +1,94 @@
+#include<string>
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val = 0;
+    TreeNode* left = NULL;
+    TreeNode* right = NULL;
+    //TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string add{};
+        if (root == NULL)
+            return string("N,");
+        add += to_string(root->val);
+        add += ",";
+        add += serialize(root->left);
+        //add += to_string(root->val);
+        add += serialize(root->right);
+        //add += to_string(root->val);
+        return add;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        string* result_data = new string[data.size()];
+        int begin = 0;
+        int l = 0;
+        int count = 0;
+        if (data.size() == 0)
+            return NULL;
+        for (int i = 0;i < data.size();i++)
+        {
+            if (data[i] == ',')
+            {
+                string s(data, begin, i - begin);
+                result_data[l++] = s;
+                begin = i + 1;
+                count++;
+            }
+        }
+        
+        queue<string> result_queue;
+        for (int i = 0;i < count;i++)
+            result_queue.push(result_data[i]);
+
+        TreeNode* result = from_queue(result_queue);
+        return result;
+    }
+
+    TreeNode* from_queue(queue<string>& result_queue) {
+        string root = result_queue.front();
+        result_queue.pop();
+        if (root == string("N"))
+            return NULL;
+        TreeNode* result = new TreeNode;
+        result->val = stoi(root);
+        result->left = from_queue(result_queue);
+        result->right = from_queue(result_queue);
+        return result;
+    }
+};
+
+void main() {
+    TreeNode* root = new TreeNode;
+    root->val = 1;
+    TreeNode* child_1 = new TreeNode;
+    child_1->val = 2;
+    root->left = child_1;
+    TreeNode* child_2 = new TreeNode;
+    child_2->val = 3;
+    root->right = child_2;
+    TreeNode* child_3 = new TreeNode;
+    child_3->val = 4;
+    child_2->left = child_3;
+    TreeNode* child_4 = new TreeNode;
+    child_4->val = 5;
+    child_2->right = child_4;
+    Codec codec;
+    string root_code= codec.serialize(root);
+    cout << root_code << endl;
+    cout << codec.serialize(codec.deserialize(root_code)) << endl;
+}
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
--- README.md
+++ README.md
@@ -2,9 +2,23 @@
 
 **算法无性能可言，仅仅只是实现！**
 
+**use C++ and Python!**
+
 ----
 
-## 001Sum_of_two_numbers
+- [001Sum_of_two_numbers](#1)
+
+- [002Sum_of_three_numbers](#2)
+
+- [003after_array_closest_target](#2)
+
+- [004longest_common_prefix](#2)
+
+- [005Serialization_and_deserialization_of_binary_trees](#2)
+
+----
+
+## <h2 id="1">001Sum_of_two_numbers</h2>
 
 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
 
@@ -17,7 +31,7 @@
 
 ----
 
-## 002Sum_of_three_numbers
+## <h2 id="2">002Sum_of_three_numbers</h2>
 
 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
 
@@ -35,7 +49,7 @@
 
 ----
 
-## 003after_array_closest_target
+## <h2 id="3">003after_array_closest_target</h2>
 
 给你一个整数数组 arr 和一个目标值 target ，请你返回一个整数 value ，使得将数组中所有大于 value 的值变成 value 后，数组的和最接近  target （最接近表示两者之差的绝对值最小）。
 
@@ -66,7 +80,7 @@
 
 ----
 
-## 004longest_common_prefix
+## <h2 id="4">004longest_common_prefix</h2>
 
 编写一个函数来查找字符串数组中的最长公共前缀。
 如果不存在公共前缀，返回空字符串 ""。
@@ -83,5 +97,29 @@
     解释: 输入不存在公共前缀。
 
 **说明:** 所有输入只包含小写字母 a-z。
+
+----
+
+## <h2 id="5">005Serialization_and_deserialization_of_binary_trees</h2>
+
+序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
+
+请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+
+示例:
+
+    你可以将以下二叉树：
+
+        1
+       / \
+      2   3
+         / \
+        4   5
+
+    序列化为 "[1,2,3,null,null,4,5]"
+
+**提示**: 这与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+
+**说明**: 不要使用类的成员 / 全局 / 静态变量来存储状态，你的序列化和反序列化算法应该是无状态的。
 
 ----
